8e237cd6b160f410d8b078c0002f5cb7e7a4d37f
rohxn-rai <rohan.rai932@outlook.com>
2025-04-09 11:21:21 -0400

    added routing config

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const fs = require("fs");
 
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end("Hello from the server!");
+  const pathname = req.url;
+
+  if (pathname === "/" || pathname === "/overview") {
+    res.end("This is the OVERVIEW page!");
+  } else if (pathname === "/product") {
+    res.end("This is the PRODUCT page!");
+  } else if (pathname === "/api") {
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {});
+    res.end("API!");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "Hello-World!",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
